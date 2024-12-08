0d43607a2976354ab818ede4824d809203d184e7
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 09:45:32 +0000

    fixed for loop

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ RawSerial pc(USBTX,USBRX,9600);
 int main()
 {
     float ax,ay,az;
-    pc.printf("Acceleration Sensor\r\n");
     eps.turn_on_regulator();
     sensor.set_up();
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 50; i++){
         sensor.sen_acc(&ax,&ay,&az);
         pc.printf("acc : %f,%f,%f\r\n",ax,ay,az);
         wait_ms(1000);
