5ae7bd648e117439738839284b263beb88d2fdaa
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 09:35:53 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include "mbed.h"
-#include "HEPTA_EPS.h"
-#include "HEPTA_SENSOR.h"
-HEPTA_EPS eps(PA_0,PA_4);
-HEPTA_SENSOR sensor(PA_7,PB_7,PB_6,0xD0);
+#include "LITE_EPS.h"
+#include "LITE_SENSOR.h"
+LITE_EPS eps(PA_0,PA_4);
+LITE_SENSOR sensor(PA_7,PB_7,PB_6);
 RawSerial pc(USBTX,USBRX,9600);
 int main()
-{   
+{
     float ax,ay,az;
-    pc.printf("Acceleration Sensor\r\n");
-    eps.turn_on_regulator(); //Turn on 3.3V converter
-    sensor.setup();
-    for(int i=0;i<50;i++){
-        //
+    eps.turn_on_regulator();
+    sensor.set_up();
+    for(int i = 0; i < 10; i++){
         sensor.sen_acc(&ax,&ay,&az);
-        pc.printf("%f,%f,%f\r\n",ax,ay,az);
+        pc.printf("acc : %f,%f,%f\r\n",ax,ay,az);
         wait_ms(1000);
     }
 }
